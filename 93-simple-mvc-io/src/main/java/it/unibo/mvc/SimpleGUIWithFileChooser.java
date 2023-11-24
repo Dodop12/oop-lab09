a72ea85a0a4d9dcc8baa5549a84869c13583d3cf
Dodop12 <dodop2002pierfe@gmail.com>
2023-11-24 15:24:24 +0100

    FileChooser ctor

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,7 +1,18 @@
 package it.unibo.mvc;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,6 +20,60 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUIWithFileChooser {
 
+    private static final int PROPORTION = 3;
+
     private final JFrame frame = new JFrame();
 
+    public SimpleGUIWithFileChooser(final Controller controller) {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+
+        final JPanel browser = new JPanel();
+        browser.setLayout(new BorderLayout());
+        canvas.add(browser, BorderLayout.NORTH);
+
+        final JTextField browseField = new JTextField();
+        browser.add(browseField, BorderLayout.CENTER);
+
+        final JButton browseButton = new JButton("Browse...");
+        browser.add(browseButton, BorderLayout.LINE_END);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(canvas);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.saveString(textArea.getText());
+                    System.out.println("Successfully saved on " + controller.getFilePath());
+                    JOptionPane.showMessageDialog(frame, "Successfully saved on " + controller.getFilePath());
+                } catch (final IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "Error occurred while trying to save",
+                            JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+
+        frame.setLocationByPlatform(true);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new SimpleGUIWithFileChooser(new Controller()).display();
+    }
 }
