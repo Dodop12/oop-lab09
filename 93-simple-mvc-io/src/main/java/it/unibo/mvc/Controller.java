7c51c505e67a8d4fef236727b002353e34b387bf
Dodop12 <dodop2002pierfe@gmail.com>
2023-11-24 10:09:36 +0100

    Controller: added getters, setters and save

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String SEP = File.separator;
+
+    private File file;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getFilePath() {
+        return this.file.getPath();
+    }
+
+    public void saveString(final String string) throws IOException {
+        try (final PrintStream ps = new PrintStream(file, StandardCharsets.UTF_8)) {
+            ps.println(string);
+        }
+    }
 
 }
