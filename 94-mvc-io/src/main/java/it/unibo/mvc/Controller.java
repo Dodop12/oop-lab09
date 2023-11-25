14f6cf9048854ed16e9ce656e3ecb5a4eae19144
Edoardo Pierfederici <dodop2002pierfe@gmail.com>
2023-11-25 16:15:11 +0100

    added javadoc comments

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -3,14 +3,34 @@ package it.unibo.mvc;
 import java.util.List;
 
 /**
- *
+ * Application controller responsible of the I/O access. Print the strings given
+ * in standard output.
  */
 public interface Controller {
+
+    /**
+     * Sets the next string to print
+     * 
+     * @throws NullPointerException if the string is null
+     * @param nextString
+     * 
+     */
     void setNextStringToPrint(String nextString);
 
+    /**
+     * @return the next string to print
+     */
     String getNextStringToPrint();
 
+    /**
+     * @return the list of all the strings printed
+     */
     List<String> getPrintedStringsHistory();
 
+    /**
+     * Print the last string set
+     * 
+     * @throws IllegalStateException if no strings are set
+     */
     void printCurrentString();
 }
